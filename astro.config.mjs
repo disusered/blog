67982d64dbad73ae3caaabf492250e3c19559013
Carlos Rosquillas <crosquillas@gmail.com>
2023-09-19 15:41:03 -0700

    Add plugin to generate robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { SITE_URL } from "./src/consts";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE_URL,
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), robotsTxt()]
 });
