fcf8039c859bba1c91ee04bef7bafc3893199d75
Carlos Rosquillas <crosquillas@gmail.com>
2023-09-21 12:37:40 -0700

    Use alt image text in schemas

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,4 @@
 export const SITE_URL = "https://disusered.netlify.app/";
 export const SITE_TITLE = "Carlos Antonio";
-export const SITE_DESCRIPTION = "Nothing and everything.";
+export const SITE_DESCRIPTION = "Personal website for Carlos Antonio";
+export const TWITTER_USERNAME = "@disusered";
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,33 @@
 import { defineCollection, z } from "astro:content";
 
-const blog = defineCollection({
-  // Type-check frontmatter using a schema
-  schema: z.object({
+const imageWithAlt = z.object({
+  heroImage: z.string(),
+  heroImageAlt: z.string(),
+});
+
+const noImage = z.object({
+  heroImage: z.undefined(),
+  heroImageAlt: z.undefined(),
+});
+
+const optionalHeroImage = imageWithAlt.or(noImage);
+
+const schema = z
+  .object({
     title: z.string(),
     description: z.string(),
     // Transform string to Date object
     pubDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
     tags: z.array(z.string()).optional(),
-  }),
+  })
+  .and(optionalHeroImage);
+
+const blog = defineCollection({
+  // Type-check frontmatter using a schema
+  schema,
 });
 
 export const collections = { blog };
+
+// "heroImage and heroImageAlt must be both defined or both undefined",
