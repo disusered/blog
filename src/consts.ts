84c2e2c0c6e1475f121943cb3d4a6b8ae1d7feb4
Carlos Rosquillas <crosquillas@gmail.com>
2023-09-28 19:01:25 -0700

    Set production URL to personal domain

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
-export const SITE_URL = "https://disusered.netlify.app/";
+export const SITE_URL = "https://disusered.com";
 export const SITE_TITLE = "Carlos Antonio";
 export const SITE_DESCRIPTION = "Personal website for Carlos Antonio";
 export const TWITTER_USERNAME = "@disusered";
